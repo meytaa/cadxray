d75ff1fc83d3efbe57a180e9a9a610281ab96673
meytaa <meyta_iri@bitbucket.org>
2023-01-16 19:44:35 -0500

    models added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import yaml
 from dataloader import read_data
 import torch
 from assemble import compile_nets
-
+#github token: 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
 
 def main(config):
 
